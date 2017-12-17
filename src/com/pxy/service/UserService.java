c02d36f2bc50136e1e87e89c1ad9104d4763c03f
pengpeng1314 <2982602715@qq.com>
2017-12-17 14:28:33 +0800

    Revert "add all"
    This reverts commit 4f45c83b08b2afc764db1d2ee94894455ae345d5.

--- src/com/pxy/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.pxy.service;
-
-import com.pxy.entity.User;
-
-public interface UserService {
-	public boolean checkUser(String username,String password);
-	public boolean hasSameName(String username);
-	public int addUser(User user);
-}
